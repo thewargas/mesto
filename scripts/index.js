2219a24a48390ddd9e636175ebcf000becd9c4fa
Vasiliy Lavrov <thewargas@mail.ru>
2022-10-31 11:04:01 +0300

    deploy

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-let editProfile = document.querySelector(`.profile__edit-button`);
+let editButton = document.querySelector(`.profile__edit-button`);
 let popup = document.querySelector(`.popup`);
 
 let nameInput = document.querySelector(`.popup__input_type_name`);
@@ -7,9 +7,9 @@ let jobInput = document.querySelector(`.popup__input_type_job`);
 let nameTitle = document.querySelector(`.profile__title`);
 let jobTitle = document.querySelector(`.profile__subtitle`);
 
-let closePopup = document.querySelector(`.popup__close-button`);
+let closeButton = document.querySelector(`.popup__close-button`);
 
-let submitEdit = document.querySelector(`.popup__form`);
+let popupForm = document.querySelector(`.popup__form`);
 
 let likeElements = document.querySelectorAll(`.element__like-button`);
 
@@ -30,9 +30,9 @@ function formSubmitHandler(evt) {
   popupClose();
 }
 
-editProfile.addEventListener(`click`, popupOpen);
-closePopup.addEventListener(`click`, popupClose);
-submitEdit.addEventListener(`submit`, formSubmitHandler);
+editButton.addEventListener(`click`, popupOpen);
+closeButton.addEventListener(`click`, popupClose);
+popupForm.addEventListener(`submit`, formSubmitHandler);
 for (let i = 0; i < likeElements.length; i++) {
   likeElements[i].addEventListener(`click`, function () {
     likeElements[i].classList.toggle(`element__like-button_active`);
