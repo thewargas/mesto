e052157243b13f29aed9234c52b7a703065aba3e
Vasiliy Lavrov <thewargas@mail.ru>
2023-01-18 13:40:18 +0300

    fix: fix link

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Card from "../components/Card.js";
 import FormValidator from "../components/FormValidator.js";
-import Section from "../components/section.js";
+import Section from "../components/Section.js";
 import PopupWithImage from "../components/PopupWithImage.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import UserInfo from "../components/UserInfo.js";
